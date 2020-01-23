f3916a9ea07e205311cd45986700a667f67eba7c
Khalid T <khalid.tissire@gmail.com>
2020-01-23 08:18:33 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
 def using_until
   levitation_force = 6
-  until answer == "no" do
-    print "Do you want this loop to continue? (y/n) "
-    answer = gets.chomp
+  until levitation_force == 10 do
+	  puts "Wingardium Leviosa"
+	  levitation_force += 1
   end
 end
-
