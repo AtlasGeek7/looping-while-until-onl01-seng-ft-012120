60ced88053d0871d6ad9a74741b2cb77c5cb97c0
Khalid T <khalid.tissire@gmail.com>
2020-01-23 08:17:44 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+  until answer == "no" do
+    print "Do you want this loop to continue? (y/n) "
+    answer = gets.chomp
+  end
 end
 
